f478a25cf50338a5e5171c8e179ac73a93084cd0
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-06-07 10:11:16 -0700

    adjust file naming

--- src/hookSample.test.js
+++ src/hookSample.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 //react-test-renderer to get instance of component
 import renderer from 'react-test-renderer';
-import HookSample from './hookSample';
+import HookSample from './HookSample';
 
 test('State Testing', () => {
     // see hookCounter.test.js
